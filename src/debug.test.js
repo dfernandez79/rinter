df5fda7305db6ea140a4be3739d995a2f6db82b2
Diego Fernandez <diegof79@gmail.com>
2019-04-16 01:27:42 -0300

    Added tests for VERBOSE and SILENT

--- src/debug.test.js
+++ src/debug.test.js
@@ -59,3 +59,50 @@ test('trace state changes from controller', () => {
 
   expect(buff).toEqual([{ value: 2 }, { value: 3 }]);
 });
+
+test('VERBOSE uses console by default', () => {
+  const output = [];
+  const log = jest.fn((...args) => output.push(args));
+  const previousConsole = global.console;
+  global.console = { log };
+
+  const testController = debug(
+    controller({
+      initialState: 0,
+      mutators: {
+        increment: n => n + 1,
+      },
+    })()
+  );
+
+  testController.increment();
+  try {
+    expect(log).toHaveBeenCalled();
+    expect(output).toEqual([[1]]);
+  } finally {
+    global.console = previousConsole;
+  }
+});
+
+test('SILENT never uses console', () => {
+  const log = jest.fn();
+  const previousConsole = global.console;
+  global.console = { log };
+
+  const testController = debug(
+    controller({
+      initialState: 0,
+      mutators: {
+        increment: n => n + 1,
+      },
+    })(),
+    debug.SILENT
+  );
+
+  testController.increment();
+  try {
+    expect(log).not.toHaveBeenCalled();
+  } finally {
+    global.console = previousConsole;
+  }
+});
